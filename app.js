74eb65a37ad2a87a1eb9dc9d19c0aa19a1174316
lizzaaabb <lbogveradze12@gmail.com>
2024-08-20 00:52:56 +0400

    slider

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const menu = document.getElementById("menu-toggle")
 const sidebar = document.getElementById("sidebar")
+const slides = document.querySelectorAll(".sliderimg")
+let slideIndex = 0;
+let intervalId = null;
 
 
 menu.onclick = function(){
@@ -15,4 +18,52 @@ menu.onclick = function(){
         slideshow.classList.add("slide-in");
     }
     
+}
+
+const galleryImages = [
+    'pics/inter1.jpg',
+    'pics/inter2.jpg',
+    'pics/inter3.jpg',
+    'pics/inter4.jpg',
+    'pics/inter5.jpg'
+];
+
+galleryImages.forEach((imageSrc) => {
+    const img = new Image();
+    img.src = imageSrc;
+});
+
+
+initializeSlider();
+
+
+function initializeSlider(){
+    slides[slideIndex].classList.add("displaySlide")
+    //intervalId = setInterval(nextSlide, 5000)
+
+}
+function showSlide(index){
+
+    if(index >= slides.length){
+        slideIndex = 0;
+    }else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide =>{
+        slide.classList.remove("displaySlide");
+    })
+    slides[slideIndex].classList.add("displaySlide")
+
+}
+function prevSlide(){
+    clearInterval(intervalId)
+    slideIndex --;
+    showSlide(slideIndex)
+
+}
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+
 }
